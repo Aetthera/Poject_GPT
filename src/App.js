61004ae1bf987afdad101b0a60419a62bc70c61b
Alena <alenabelovadmvn@gmail.com>
2023-05-14 19:00:44 -0400

    header component built (no final style)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import'./App.css';
 const App = () => {
   return (
     <div className='App'>
-        <div className='gardient__bg'>
+        <div className='gradient__bg'>
             <Navbar />
             <Header />
         </div>
